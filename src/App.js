3e9c40abf3a5d39b24e8999008e6711a6139e801
A.C.E <amienghemenchristian948@gmail.com>
2022-11-22 02:11:07 +0100

    broken the homepage into smaller components and finished styling the menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Homepage from './components/Homepage';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Homepage/>
     </div>
   );
 }
