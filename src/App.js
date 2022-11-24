be59a78aff80c18815b3e7e7c5f6515676c02784
A.C.E <amienghemenchristian948@gmail.com>
2022-11-24 20:07:05 +0100

    update the menu-items to navigate with useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/Homepage";
 
-import './App.css';
-import Homepage from './pages/homepage/Homepage';
+const HatsPage = () => (
+  <dvi>
+    <h1>HATS PAGE</h1>
+  </dvi>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,13 +1,17 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom'
 
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+
+  let navigate = useNavigate()
+
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        style={{ backgroundImage: `url(${imageUrl})`}}
       />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
